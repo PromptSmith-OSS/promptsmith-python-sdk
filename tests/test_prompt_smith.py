140d3925983e4857213829ff896e21c546044916
tim-hub <tim-hub@users.noreply.github.com>
2024-10-05 21:00:30 +1300

    add a basic test case for client get defined

--- tests/test_prompt_smith.py
+++ tests/test_prompt_smith.py
@@ -1,3 +1,10 @@
+# test prompt smith client
 
 
+from python_sdk.prompt_smith import PromptSmith
 
+
+def test_prompt_smith():
+    prompt_smith = PromptSmith("http://localhost:8000", "a")
+    # prompt smith get defined
+    assert prompt_smith.get_prompt is not None
