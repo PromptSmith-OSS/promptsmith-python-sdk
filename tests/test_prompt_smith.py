5810da1ff212fb5a374f5a9b328e4cc5aa7b5cc9
tim-hub <tim-hub@users.noreply.github.com>
2024-10-06 17:52:13 +1300

    enable async function and optimise caching

--- tests/test_prompt_smith.py
+++ tests/test_prompt_smith.py
@@ -5,6 +5,12 @@ from promtsmithsdk.prompt_smith import PromptSmith
 
 
 def test_prompt_smith():
+    """
+    The basic test for prompt smith client
+    :return:
+    """
     prompt_smith = PromptSmith("http://localhost:8000", "a")
     # prompt smith get defined
     assert prompt_smith.get_prompt is not None
+    # prompt smith get async defined
+    assert prompt_smith.get_prompt_async is not None
